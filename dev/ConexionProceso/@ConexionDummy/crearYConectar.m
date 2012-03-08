1728da6947c12f545b5ac6438da4e239620f5448
pablodroca@gmail.com <pablodroca@gmail.com@623c809d-8309-da0e-dcfc-169574685abb>
2012-03-08 03:29:45 +0000

    Correcciones en conexionDummy. Agregado de ejemplo con timers para correr simulink en background

--- dev/ConexionProceso/@ConexionDummy/crearYConectar.m
+++ dev/ConexionProceso/@ConexionDummy/crearYConectar.m
@@ -1,4 +1,5 @@
 function self = crearYConectar(self)
+    fprintf('ConexionDummy: Conectado\n');
     self.conectado = 1;
 end
 
--- dev/main.m
+++ dev/main.m
@@ -7,40 +7,41 @@ addpath('GraficoProceso');
 addpath('SimulinkModels');
 
 
-SAMPLES_QTY = 200;
+SAMPLES_QTY = 4;
 
 global conexion
-%load_system('.\SimulinkModels\UnTanque.mdl');
+global muestraTimer
+load_system('UnTanque.mdl');
     
 muestras = [];
-modoDummy = 2;
-if modoDummy == 0
+modoEjecucion = 0;
+
+if modoEjecucion == 0
     conexion = ConexionDummy(3);
     conexion = crearYConectar(conexion);
     conexion = establecerUltimaMuestraDummy(conexion, 10);
-
-
-    set_param('UnTanque', 'SimulationCommand', 'start');
-    for i = 1:SAMPLES_QTY
-        pause(0.3);
-        muestra = 10 + i;
-        conexion = establecerUltimaMuestraDummy(conexion, muestra);
-        muestras = [muestras muestra];
-    end
-    set_param('UnTanque', 'SimulationCommand', 'stop');
-    desconectar(conexion);
-elseif modoDummy == 1
+    
+    muestraTimer = 0;
+    timer1 = timer('StartDelay',0, 'TimerFcn', ...
+        'fprintf(''Simulation Started\n'');set_param(''UnTanque'', ''SimulationCommand'', ''start'');');
+    timer2 = timer('StartDelay',10, 'TimerFcn', ...
+        'fprintf(''Simulation Stoped\n''); set_param(''UnTanque'', ''SimulationCommand'', ''stop''); conexion = desconectar(conexion); delete(timer1);delete(timer2);delete(timer3);');
+    timer3 = timer('Period',0.5, 'ExecutionMode','fixedDelay', 'TimerFcn', ...
+        'fprintf(''%d'', muestraTimer); muestraTimer =  muestraTimer + 1; conexion = establecerUltimaMuestraDummy(conexion, muestraTimer); muestras = [muestras muestraTimer];');
+    start(timer1); start(timer2); start(timer3);
+    %wait(timer1); wait(timer2); stop(timer3);
+    %delete(timer1); delete(timer2); delete(timer3);
+elseif modoEjecucion == 1
     conexion = Conexion(3);
     conexion = crearYConectar(conexion);
-    set_param('UnTanque', 'SimulationCommand', 'start');
-    for i = 1:SAMPLES_QTY
-        pause(0.3);
-        [conexion, muestra] = obtenerMuestra(conexion);
-        fprintf('Muestra obtenida: %f', muestra);
-        muestras = [muestras muestra];
-    end
-    set_param('UnTanque', 'SimulationCommand', 'stop');
-    desconectar(conexion);
+
+    timer1 = timer('StartDelay',0, 'TimerFcn', ...
+        'fprintf(''Simulation Started\n'');set_param(''UnTanque'', ''SimulationCommand'', ''start'');');
+    timer2 = timer('StartDelay',10, 'TimerFcn', ...
+        'fprintf(''Simulation Stoped\n''); set_param(''UnTanque'', ''SimulationCommand'', ''stop''); conexion = desconectar(conexion); delete(timer1);delete(timer2);delete(timer3);');
+    timer3 = timer('Period',0.5, 'ExecutionMode','fixedDelay', 'TimerFcn', ...
+        '[conexion, muestraTimer] = obtenerMuestra(conexion); fprintf(''Muestra obtenida: %f\n'', muestraTimer); muestras = [muestras muestraTimer];');
+    start(timer1); start(timer2); start(timer3);
 else
     tic
     conexion = Conexion(3);
