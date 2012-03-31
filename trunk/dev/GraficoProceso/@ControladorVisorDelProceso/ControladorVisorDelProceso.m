e03032bc232689465db7e8e348dab3482431b520
pablodroca@gmail.com <pablodroca@gmail.com@623c809d-8309-da0e-dcfc-169574685abb>
2012-03-31 02:15:48 +0000

    Agregado de SeleccionDeModeloGUI. Conexiones de controladores con sus vistas. Agregado del código de timing en controlador de VisorDelProceso.

--- trunk/dev/GraficoProceso/@ControladorVisorDelProceso/ControladorVisorDelProceso.m
+++ trunk/dev/GraficoProceso/@ControladorVisorDelProceso/ControladorVisorDelProceso.m
@@ -1,5 +1,10 @@
-function [ self ] = ControladorVisorDelProceso( )
-   self = struct();
+function [ self ] = ControladorVisorDelProceso(wVisorDelProceso)
+   self = struct('vista', wVisorDelProceso);
    self = class(self, 'ControladorVisorDelProceso');
+   global ControladorVisorDelProcesoSelf
+   ControladorVisorDelProcesoSelf = self;
+   timer3 = timer('Period',0.5, 'ExecutionMode','fixedDelay', 'TimerFcn', ...
+        'fprintf(''%d'', muestraTimer); muestraTimer =  muestraTimer + 1; conexion = establecerUltimaMuestraDummy(conexion, muestraTimer); muestras = [muestras muestraTimer];procesarMuestra(ControladorVisorDelProcesoSelf, muestraTimer);');
+   start(timer3);
 end
 
---
+++ trunk/dev/GraficoProceso/@ControladorVisorDelProceso/procesarMuestra.m
@@ -0,0 +1,3 @@
+function procesarMuestra( self, muestra )
+fprintf('muestra a procesar: %f\n', muestra);
+agregarMuestra(self.vista, 100, muestra);
