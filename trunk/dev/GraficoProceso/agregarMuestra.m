ce4495e5e44d51fc084f81d5b23e386037f31d45
ezequieldidonato@gmail.com <ezequieldidonato@gmail.com@623c809d-8309-da0e-dcfc-169574685abb>
2012-04-20 04:41:00 +0000

    Visor proceso graafico separado

--- trunk/dev/GraficoProceso/agregarMuestra.m
+++ trunk/dev/GraficoProceso/agregarMuestra.m
@@ -4,11 +4,19 @@ handles = guihandles(visorDelProceso);
 set(handles.txtMuestra, 'String', instante);
 set(handles.txtNivel, 'String', muestra);
 
-try %TODO remover el trycatch
-	axes(handles.axesProceso);
-
-	plot([instante, instante+1, instante+2, instante+3, instante+4], [muestra,  muestra*2, muestra/2, muestra/3, muestra*1.5])
+try %TODO remover el trycatc
+	%axes(handles.axesVisorProceso);
+    %line('XData',[instante, instante+1, instante+2, instante+3, instante+4], 'YData', [muestra,  muestra*2, muestra/2, muestra/3, muestra*1.5]);
+    disp('Grafico instante: ')
+    instante
+    disp('Muestra: ');
+    muestra
+    %figure(visorDelProceso);
+    %axes(visorDelProceso.axesisorProceso);
+    plot([instante, instante+1, instante+2, instante+3, instante+4], [muestra,  muestra*1.2, muestra*1.3, muestra*1.5, muestra*3]);
 catch
-%hacer nada por ahora
+ exception = lasterr;
+ disp('Excepcion encontrada');
+ disp(exception);
 end
 end
