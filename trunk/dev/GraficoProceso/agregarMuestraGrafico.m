9e6317d0a96cc9873f200b31a354c5d2a219e91d
ezequieldidonato@gmail.com <ezequieldidonato@gmail.com@623c809d-8309-da0e-dcfc-169574685abb>
2012-06-02 21:10:40 +0000

    Grafico Ampliado Toogle buttons para zoom y grillas.

--- trunk/dev/GraficoProceso/agregarMuestraGrafico.m
+++ trunk/dev/GraficoProceso/agregarMuestraGrafico.m
@@ -1,4 +1,4 @@
 function agregarMuestraGrafico(graficoDelProceso, instantes, muestras)
     handles = guihandles(graficoDelProceso);
-    line(instantes, muestras, 'Parent', handles.axesGraficoDelProceso);
+    %line(instantes, muestras, 'Parent', handles.axesGraficoDelProceso);
 end
