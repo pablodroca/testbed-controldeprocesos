283f3b6578537c6f16f22e293de110db0d9881dd
pablodroca@gmail.com <pablodroca@gmail.com@623c809d-8309-da0e-dcfc-169574685abb>
2012-07-04 11:30:45 +0000

    Correccion de modo reproducción. Ajuste de timers para conseguir un gráfico más preciso

--- dev/GraficoProceso/@ControladorVisorDelProceso/notificarEventosGrabacion.m
+++ dev/GraficoProceso/@ControladorVisorDelProceso/notificarEventosGrabacion.m
@@ -1,10 +1,10 @@
-function [ self ] = notificarEventosGrabacion( self )
+function self = notificarEventosGrabacion( self )
     global conexion
     
     instante = getUltimosInstantes(self.proceso, 1);
     instante = instante(1);
     
-   cambioConfig = getCambioConfiguracionPorInstante(conexion, instante);
+    cambioConfig = getCambioConfiguracionPorInstante(conexion, instante);
     comentario = getComentarioPorInstante(conexion, instante);
     if ~isempty(cambioConfig)
         registrarCambioConfiguracion(self.vista, cambioConfig);
